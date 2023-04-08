3dfa9ae9690220f18f007573b47895fc9f928eaa
Valentin <valentinlp.20@gmail.com>
2023-04-08 15:42:13 -0300

    agregue funcion duelo para juego de guerrra

--- modulos/guerra.py
+++ modulos/guerra.py
@@ -5,6 +5,7 @@ import random
 
 
 def puntos (valor1):
+    """Calcula un valor de puntos a cada carta para su comparacion"""
     
     if valor1 in ['A','J','Q','K']:
         if valor1 =='A':
@@ -23,36 +24,41 @@ def puntos (valor1):
 
 
 
-
+#_______________________________________________________________COLA DOBLE
 class Cola_doble:
     
     def __init__(self):
         
         self.lista=LDE()
+        self.tamanio=0
     
     def agregar_in(self, item):
         self.lista.agregar_al_inicio(item)
+        self.tamanio+=1
     
     def agregar_fin(self,item):
         self.lista.agregar_al_final(item)
+        self.tamanio+=1
         
     def sacar_in(self):
+        self.tamanio-=1
         return self.lista.extraer(0)
     
     def sacar_fin(self):
+        self.tamanio-=1
         return self.lista.extraer(self.lista.tamanio -1)
     
     def __str__(self):
         return str(self.lista)
     
     def __len__(self):
-        return self.lista.tamanio
+        return self.tamanio
     
     
     
         
     
-
+#_________________________________________________________________CARTA
 class Carta:
     
     def __init__(self,palo, valor):
@@ -73,8 +79,13 @@ class Carta:
     def __lt__(self, otro):
        
         return puntos(self.valor)<puntos(otro.valor)
+    
+    def __eq__(self, otro):
+        return puntos(self.valor)==puntos(otro.valor)
+        
         
 
+#_____________________________________________________________________JUEGO 
 class JuegoGuerra:
     
     def __init__(self):
@@ -119,6 +130,28 @@ class JuegoGuerra:
 
         self.jugador1=jugador1
         self.jugador2=jugador2
+        self.turnos=0
+
+    def duelo(self):
+        
+        carta1=self.jugador1.sacar_fin()
+        carta2=self.jugador2.sacar_fin()
+        
+        
+        
+        if carta1>carta2:
+            self.jugador1.agregar_in(carta1)
+            self.jugador1.agregar_in(carta2)
+            
+        elif carta1<carta2:
+            self.jugador2.agregar_in(carta1)
+            self.jugador2.agregar_in(carta2)
+            
+        carta1=str(carta1)+' '+str(carta2)
+        print(carta1.center(60))
+            
+        
+        
         
 
 
@@ -135,6 +168,28 @@ if __name__ == '__main__':
     print('JUGADOR 2')
     print(juego.jugador2)
     print('Tamaño: ',len(juego.jugador2))
+    
+    
+    print('')
+    
+    juego.duelo()
+    
+    print('')
+    
+    print('JUGADOR 1:')
+    print(juego.jugador1)
+    print('Tamaño: ',len(juego.jugador1))
+    
+    print('')
+    print('JUGADOR 2')
+    print(juego.jugador2)
+    print('Tamaño: ',len(juego.jugador2))
+    
+    
+  
+    
+ 
+    
     
     
 
